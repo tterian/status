0e51dfb4918296fa4db18fa8882b4caa5b79d571
Tigran Terian <tigran.terian@gmail.com>
2015-03-07 11:17:48 +0400

    Added models: users, stats

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,2 +1,8 @@
 class Stat < ActiveRecord::Base
+	belongs_to :user
+
+	validates :status,
+				presence: true,
+				length: { minimum: 3, maximum: 40 }
+
 end
--- db/migrate/20150307061042_create_stats.rb
+++ db/migrate/20150307061042_create_stats.rb
@@ -1,10 +1,11 @@
 class CreateStats < ActiveRecord::Migration
   def change
     create_table :stats do |t|
-      t.string :owner
       t.string :status
+      t.references :user, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :stats, :users
   end
 end
